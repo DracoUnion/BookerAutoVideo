7c029e5a28d7220d9736a1723b3a38711082d118
wizardforcel <562826179@qq.com>
2022-12-17 16:02:37 +0800

    2022-12-17 16:02:37

---
+++ BookerAutoVideo/autovideo_config.py
@@ -0,0 +1,11 @@
+config = {
+    'name': '',
+    'format': 'mp4',
+    'bgm': '',
+    'bgImg': '',
+    'imgSize': [1280, 768],
+    'padding': 0,
+    'contents': [],
+    'header': '', 
+    'footer': '',
+}
