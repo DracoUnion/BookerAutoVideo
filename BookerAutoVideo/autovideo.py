11a83cf5228a46f21616c7facb4066c76f13f61e
wizardforcel <562826179@qq.com>
2022-12-17 16:35:09 +0800

    2022-12-17 16:35:09

--- BookerAutoVideo/autovideo.py
+++ BookerAutoVideo/autovideo.py
@@ -4,6 +4,8 @@ from paddlespeech.cli.tts.infer import TTSExecutor
 from .autovideo_config import config
 from EpubCrawler.util import request_retry
 
+DIR = path.abspath(path.dirname(__file__))
+
 def md2playbook(args):
     fname = args.fname
     if not fname.endswith('.md'):
@@ -48,6 +50,20 @@ def autovideo(args):
     ]
     
     # TODO 剪裁图片
+    
+    # 如果第一张不是图片，则提升第一个图片
+    idx = -1
+    for i, c in enumerate(config['contents']):
+        if c['type'].startswith('image:'):
+            idx = i
+            break
+    if idx == -1:
+        print('内容中无图片，无法生成视频')
+        return
+    if idx != 0:
+        c = config['contents'][idx]
+        del config['contents'][idx]
+        config['contents'].insert(0, c)
     
     # 组装视频
     
