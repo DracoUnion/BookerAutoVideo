b20bc8db7ccadebc5de603c3a7db345ffa0d869b
wizardforcel <562826179@qq.com>
2022-12-17 16:54:53 +0800

    2022-12-17 16:54:53

--- BookerAutoVideo/autovideo.py
+++ BookerAutoVideo/autovideo.py
@@ -1,5 +1,7 @@
 import yaml
 from os import path
+import librosa
+from io import BytesIO
 from paddlespeech.cli.tts.infer import TTSExecutor
 from .autovideo_config import config
 from EpubCrawler.util import request_retry
@@ -11,7 +13,11 @@ def md2playbook(args):
     if not fname.endswith('.md'):
         print('请提供 Markdown 文件')
         return
-        
+
+def audio_len(data):
+    y, sr = librosa.load(BytesIO(data))
+    return librosa.get_duration(y=y, sr=sr)
+
 def exec_tts(text):
     tts = TTSExecutor()
     fname = path.join(tempfile.gettempdir(), uuid.uuid4().hex + '.wav')
@@ -19,20 +25,10 @@ def exec_tts(text):
     data = open(fname, 'rb').read()
     os.unlink(fname)
     return data
-        
-def autovideo(args):
-    cfg_fname = args.fname
-    if not cfg_fname.endswith('.yml'):
-        print('请提供 YAML 文件')
-        return
-    user_cfg = yaml.safe_load(cfg_fname)
-    config.update(user_cfg)
-    
-    if not config['contents']:
-        print('内容为空，无法生成')
-        return
-        
-    # 素材预处理
+
+# 素材预处理
+def preproc_asset(config):
+    # 加载或生成内容
     for cont in config['contents']:
         if cont['type'].endswith(':file'):
             fname = path.join(cfg_fname, cont['value'])
@@ -64,6 +60,41 @@ def autovideo(args):
         c = config['contents'][idx]
         del config['contents'][idx]
         config['contents'].insert(0, c)
+
+# 内容成帧
+def contents2frame(contents):
+    frames = []
+    last_img = None
+    for c in contents:
+        if c['type'].startswith['image:']:
+            last_img = c
+        elif c['type'].startswith['audio:']:
+            frames.append({
+                'image': last_img['asset'],
+                'audio': c['asset'],
+                'subtitle': c['value'] if c['type'] == 'audio:tts' else '',
+            })
+    frames = [
+        f for f in frames
+        if 'image' in f
+    ]
+    return frames
+
+def autovideo(args):
+    cfg_fname = args.fname
+    if not cfg_fname.endswith('.yml'):
+        print('请提供 YAML 文件')
+        return
+    user_cfg = yaml.safe_load(cfg_fname)
+    config.update(user_cfg)
     
+    if not config['contents']:
+        print('内容为空，无法生成')
+        return
+        
+    # 素材预处理
+    preproc_asset(config)
+    # 转换成帧的形式
+    frames = contents2frame(config['contents'])
     # 组装视频
     
