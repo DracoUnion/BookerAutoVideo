553ee0aa7e6d427dd54f494acea993638ec2af20
wizardforcel <562826179@qq.com>
2022-12-17 16:23:00 +0800

    2022-12-17 16:23:00

--- BookerAutoVideo/autovideo.py
+++ BookerAutoVideo/autovideo.py
@@ -1,4 +1,6 @@
 import yaml
+from os import path
+from paddlespeech.cli.tts.infer import TTSExecutor
 from .autovideo_config import config
 from EpubCrawler.util import request_retry
 
@@ -7,6 +9,14 @@ def md2playbook(args):
     if not fname.endswith('.md'):
         print('请提供 Markdown 文件')
         return
+        
+def exec_tts(text):
+    tts = TTSExecutor()
+    fname = path.join(tempfile.gettempdir(), uuid.uuid4().hex + '.wav')
+    tts(text=text, output=fname)
+    data = open(fname, 'rb').read()
+    os.unlink(fname)
+    return data
         
 def autovideo(args):
     cfg_fname = args.fname
@@ -20,8 +30,18 @@ def autovideo(args):
         print('内容为空，无法生成')
         return
         
-    # 读取文件
+    # 素材预处理
     for cont in config['contents']:
         if cont['type'].endswith(':file'):
-            fname = path.join(cfg_fname, cont['content'])
-            cont['
+            fname = path.join(cfg_fname, cont['value'])
+            cont['asset'] = open(fname, 'rb').read()
+        elif cont['type'].endswith(':url'):
+            cont['asset'] = request_retry('GET', cont['value'])
+        elif cont['type'] == 'audio:tts':
+            cont['asset'] = exec_tts(cont['value'])
+            
+    config['contents'] = [
+        c for c in config['contents']
+        if 'asset' in c
+    ]
+    
