3e50034c3189e3dd3fad1171e85c3673e96fdb4f
wizardforcel <562826179@qq.com>
2022-12-30 01:48:10 +0800

    2022-12-30 01:48:10

--- BookerAutoVideo/audio2txt.py
+++ BookerAutoVideo/audio2txt.py
@@ -4,9 +4,11 @@ from paddlespeech.cli.text.infer import TextExecutor
 
 def audio2txt_handle(args):
     fname = args.fname
+    # 语音识别
     model = whisper.load_model(args.model)
     r = model.transcribe(fname, fp16=False, language='Chinese')
     words = [s['text'] for s in r['segments']]
+    # 标点修正
     text_executor = TextExecutor()
     text = ''.join([
         text_executor(
@@ -16,5 +18,10 @@ def audio2txt_handle(args):
             device=paddle.get_device(),
         ) for w in words
     ])
-    open(fname + '.txt', 'w', encoding='utf8').write(text)
-    print(fname + '.txt')
+    # 排版
+    text = re.sub(r'(.{50,100}(?:。|！|？))', r'\1\n\n', text)
+    title = path.basename(fname)
+    title = re.sub(r'\.\w+$', '', title)
+    text = f'# {title}\n\n{text}'
+    open(fname + '.md', 'w', encoding='utf8').write(text)
+    print(fname + '.md')
