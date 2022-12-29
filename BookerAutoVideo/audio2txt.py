2c2172c3fca346c1552b2efa24aea53a1918a398
wizardforcel <562826179@qq.com>
2022-12-30 01:38:17 +0800

    2022-12-30 01:38:17

--- BookerAutoVideo/audio2txt.py
+++ BookerAutoVideo/audio2txt.py
@@ -5,13 +5,16 @@ from paddlespeech.cli.text.infer import TextExecutor
 def audio2txt_handle(args):
     fname = args.fname
     model = whisper.load_model(args.model)
-    word = model.transcribe(fname, fp16=False, language='Chinese')['text']
+    r = model.transcribe(fname, fp16=False, language='Chinese')
+    words = [s['text'] for s in r['segments']]
     text_executor = TextExecutor()
-    text = text_executor(
-        text=word,
-        task='punc',
-        model='ernie_linear_p3_wudao',
-        device=paddle.get_device(),
-    )
+    text = ''.join([
+        text_executor(
+            text=w,
+            task='punc',
+            model='ernie_linear_p3_wudao',
+            device=paddle.get_device(),
+        ) for w in words
+    ])
     open(fname + '.txt', 'w', encoding='utf8').write(text)
     print(fname + '.txt')
