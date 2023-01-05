ba29893fdc0fb41dad671b37bda6e04a55849974
wizardforcel <562826179@qq.com>
2023-01-06 01:11:13 +0800

    2023-01-06 01:11:13

--- BookerAutoVideo/audio2txt.py
+++ BookerAutoVideo/audio2txt.py
@@ -1,8 +1,11 @@
-import paddle
+# import paddle
 import whisper
 import re
+import traceback
+import copy
 from os import path
-from paddlespeech.cli.text.infer import TextExecutor 
+from multiprocessing import Pool
+# from paddlespeech.cli.text.infer import TextExecutor 
 
 def merge_words(words, maxl=500):
     res = []
@@ -18,7 +21,32 @@ def merge_words(words, maxl=500):
     return res
 
 def audio2txt_handle(args):
+    if path.isdir(args.fname):
+        audio2txt_dir(args)
+    else: 
+        audio2txt_file(args)
+
+def audio2txt_dir(args):
+    dir = args.fname
+    fnames = os.listdir(dir)
+    pool = Pool(args.threads)
+    for fname in fnames:
+        args = copy.deepcopy(args)
+        args.fname = path.join(dir, fname)
+        pool.apply_async(audio2txt_file_safe, [args])
+    pool.close()
+    pool.join()
+    
+def audio2txt_file_safe(args):
+    try: audio2txt_file(args)
+    except: traceback.print_exc()
+
+def audio2txt_file(args):
     fname = args.fname
+    if not path.isfile(fname):
+        print('请提供音频或视频文件')
+        return
+    print(fname)
     # 语音识别
     model = whisper.load_model(args.model)
     r = model.transcribe(fname, fp16=False, language='Chinese')
