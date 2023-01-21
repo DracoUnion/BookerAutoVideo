b137c744420938e8482cd25143dc9fc4dcf1d85d
wizardforcel <562826179@qq.com>
2023-01-21 23:17:30 +0800

    2023-01-21 23:17:30

--- BookerAutoVideo/keyframe.py
+++ BookerAutoVideo/keyframe.py
@@ -5,23 +5,34 @@ import shutil
 import math
 import sys
 
-def ext_keyframe(args):
-    fname = args.file
-    print(fname)
-    img_path = fname + "_img"
-    print(img_path)
-    if os.path.isdir(img_path):
-        shutil.rmtree(img_path)
-    os.makedirs(img_path)
+def get_keyframes(fname):
     container = av.open(fname)
 
     stream = container.streams.video[0]
     stream.codec_context.skip_frame = 'NONKEY'
 
+    res = []
     for frame in container.decode(stream):
-        fname = f'idx{frame.index}_pts{frame.pts}_dts{frame.dts}.png'
-        frame.to_image().save(
-            os.path.join(img_path, fname), 
-            compress=True,
-        )
+        bio = BytesIO()
+        frame.to_image().save(bio, format="PNG",compress=9)
+        res.append({
+            'idx': frame.index,
+            'pts': frame.pts,
+            'dts': frame.dts,
+            'data': bio.getvalue(),
+        })
+    return res
+
+def ext_keyframe(args):
+    fname = args.file
+    print(fname)
+    opath = fname + "_img"
+    print(opath)
+    if os.path.isdir(opath):
+        shutil.rmtree(opath)
+    os.mkdir(opath)
+    kfs = get_keyframes(fname)
+    for kf in kfs:
+        fname = f'idx{kf["idx"]}_pts{kf["pts"]}_dts{kf["dts"]}.png'
+        open(path.join(opath, fname), 'wb').write(kf['data'])
 
