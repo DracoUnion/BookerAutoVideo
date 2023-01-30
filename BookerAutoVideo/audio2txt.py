6fce047d07d92d3f8a8cf07ca3f9471bb66d32e2
wizardforcel <562826179@qq.com>
2023-01-31 02:10:22 +0800

    2023-01-31 02:10:22

--- BookerAutoVideo/audio2txt.py
+++ BookerAutoVideo/audio2txt.py
@@ -23,28 +23,28 @@ def merge_words(words, maxl=500):
     res.append(''.join(words[st:]))
     return res
 
-def audio2txt_handle(args):
+def video2txt_handle(args):
     if path.isdir(args.fname):
-        audio2txt_dir(args)
+        video2txt_dir(args)
     else: 
-        audio2txt_file(args)
+        video2txt_file(args)
 
-def audio2txt_dir(args):
+def video2txt_dir(args):
     dir = args.fname
     fnames = os.listdir(dir)
     pool = Pool(args.threads)
     for fname in fnames:
         args = copy.deepcopy(args)
         args.fname = path.join(dir, fname)
-        pool.apply_async(audio2txt_file_safe, [args])
+        pool.apply_async(video2txt_file_safe, [args])
     pool.close()
     pool.join()
     
-def audio2txt_file_safe(args):
-    try: audio2txt_file(args)
+def video2txt_file_safe(args):
+    try: video2txt_file(args)
     except: traceback.print_exc()
 
-def audio2txt_file(args):
+def video2txt_file(args):
     fname = args.fname
     if not (path.isfile(fname) and is_video_or_audio(fname)):
         print('请提供音频或视频文件')
@@ -53,8 +53,17 @@ def audio2txt_file(args):
     # 语音识别
     model = whisper.load_model(args.model)
     r = model.transcribe(fname, fp16=False, language='Chinese')
-    words = [s['text'] for s in r['segments']]
-    print(f'words: {words}')
+    words = [
+        {'time': s['start'], 'text': s['text']}
+        for s in r['segments']
+    ]
+    # print(f'words: {words}')
+    # 获取关键帧
+    if args.image and is_video(fname):
+        config_scene(args)
+        frames = extract_keyframe(args)
+        words += frames
+        words.sort(lambda x: x['time'])
     # 标点修正
     '''
     words = merge_words(words)
@@ -69,10 +78,28 @@ def audio2txt_file(args):
     ])
     '''
     # 排版
-    text = stylish_text('，'.join(words))
     title = path.basename(fname)
     title = re.sub(r'\.\w+$', '', title)
+    title_hash = hashlib.md5(title).hexdigest()
+    for i in range(len(words) - 1, 0, -1):
+        if 'text' in words[i] and 'text' in words[i - 1]:
+            words[i - 1]['text'] += '，' + words[i]['text']
+            del words[i]
+    imgs = {}
+    for i, w in enumerate(words):
+        if 'img' in w: 
+           imgname = f'{title_hash}_{i}.png'
+           imgs[imgname] = w['img']
+           w['text'] = f'![](img/{imgname})'
+        elif 'text' in w:
+            w['text'] = stylish_text(w['text'])
+    text = '\n\n'.join([w['text'] for w in words])
     text = f'# {title}\n\n{text}'
     nfname = re.sub(r'\.\w+$', '', fname) + '.md'
     open(nfname , 'w', encoding='utf8').write(text)
     print(nfname + '.md')
+    imgdir = path.join(path.dirname(fname), 'img')
+    for imgname, img in imgs.items():
+        img_fname = path.join(imgdir, imgname)
+        print(img_fname)
+        open(img_fname, 'wb').write(img)
