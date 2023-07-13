74a0d0f182487e2ec0556e2c4a5e97eaaf64b0fb
wizardforcel <562826179@qq.com>
2023-07-13 17:26:01 +0800

    2023-07-13 17:25:59

--- BookerAutoVideo/video2txt.py
+++ BookerAutoVideo/video2txt.py
@@ -3,6 +3,7 @@ import re
 import traceback
 import copy
 import os
+import json
 import hashlib
 from os import path
 from multiprocessing import Pool
@@ -73,7 +74,7 @@ def whisper_cpp(args):
     if not path.isfile(wav_fname):
         safe_remove(wav_fname)
         raise FileNotFoundError(f'{fname} 识别失败')
-    res = json.dumps(open(json_fname, encoding='utf8').read())
+    res = json.loads(open(json_fname, encoding='utf8').read())
     safe_remove(wav_fname)
     safe_remove(json_fname)
     return [
