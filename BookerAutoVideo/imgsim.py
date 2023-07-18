565f747c35acc764f9fda8a1d5c8927d8e3061fa
wizardforcel <wizard.z@qq.com>
2023-07-18 17:30:12 +0800

    2023-07-18 17:30:10

--- BookerAutoVideo/imgsim.py
+++ BookerAutoVideo/imgsim.py
@@ -3,9 +3,13 @@ import numpy as np
 import os
 from .util import *
 
+def ensure_grayscale(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) \
+           if img.ndim == 3 else img
+
 # 直方图余弦
 def hist_cos_sim(prev, next):
-    assert prev.ndim == 2 and next.ndim == 2
+    prev, next = ensure_grayscale(prev), ensure_grayscale(next)
     prev_hist = np.histogram(prev, bins=list(range(257)))[0]
     next_hist = np.histogram(next, bins=list(range(257)))[0]
     prev_hist = prev_hist / prev_hist.sum()
@@ -17,7 +21,7 @@ def hist_cos_sim(prev, next):
 
 # 直方图 L1
 def hist_l1_sim(prev, next):
-    assert prev.ndim == 2 and next.ndim == 2
+    prev, next = ensure_grayscale(prev), ensure_grayscale(next)
     prev_hist = np.histogram(prev, bins=list(range(257)))[0]
     next_hist = np.histogram(next, bins=list(range(257)))[0]
     max = np.fmax(prev_hist, next_hist)
@@ -26,7 +30,7 @@ def hist_l1_sim(prev, next):
 
 # 均值哈希
 def ahash(img, size=12):
-    assert img.ndim == 2
+    img = ensure_grayscale(img)
     resize = cv2.resize(img, (size, size))
     mean = resize.mean()
     # 灰度大于平均值为 1 相反为 0
@@ -35,7 +39,7 @@ def ahash(img, size=12):
     
 # 差值哈希
 def dhash(img, size=12):
-    assert img.ndim == 2
+    img = ensure_grayscale(img)
     resize = cv2.resize(img, (size, size))
     before = resize[:, :-1]
     after = resize[:, 1:]
@@ -45,7 +49,7 @@ def dhash(img, size=12):
 
 # 感知哈希
 def phash(img, size=32):
-    assert img.ndim == 2
+    img = ensure_grayscale(img)
     resize = cv2.resize(img, (size, size))
     dct = cv2.dct(np.float32(resize))
     roi = dct[0:8, 0:8]
@@ -75,6 +79,28 @@ def pixel_l1_sim(prev, next):
     diff = np.mean(np.where(max != 0, cv2.absdiff(next, prev) / max, 0))
     return 1 - diff
 
+def gs_fullness(chan):
+    freqs, _ = np.histogram(img.astype("int"), bins=range(256 + 1), density=True)
+    freqs = np.where(freqs, freqs, 1e-12)
+    return np.sum(-freqs * np.log2(freqs)) / 8
+
+def color_fullness(img): 
+    b, g, r = np.split(img.astype("int") // 16, 3, axis=-1) 
+    rgb = r * 256 + g * 16 + b
+    freqs, _ = np.histogram(rgb, bins=range(16 ** 3 + 1), density=True)
+    freqs = np.where(freqs, freqs, 1e-12)
+    return np.sum(-freqs * np.log2(freqs)) / 12
+
+def image_fullness(img):
+    if img.ndim == 3:
+        return (color_fullness(img) + gs_fullness(img)) / 2
+    else:
+        return gs_fullness(img)
+
+def fullness_sim(prev, next):
+    ifn = image_fullness(next)
+    return ifn * phash_sim(prev, next) / 2 + (1 - ifn) * pixel_l1_sim(prev, next) * 5
+
 img_sim = {
     'pixel_l1':  pixel_l1_sim,
     'pixel_cos': pixel_cos_sim,
@@ -83,6 +109,7 @@ img_sim = {
     'phash': phash_sim,
     'hist_l1':   hist_l1_sim,
     'hist_cos':  hist_cos_sim,
+    'fullness':  fullness_sim,
 }
 
 def img_sim_dir_handle(args):
