9839b291b9a82ec8ea2978ac31bf697a13c126be
wizardforcel <562826179@qq.com>
2023-08-27 21:24:40 +0800

    2023-08-27 21:24:38

--- BookerAutoVideo/util.py
+++ BookerAutoVideo/util.py
@@ -169,8 +169,7 @@ def ffmpeg_add_srt(video, srt, video_fmt='mp4'):
     # cmd = ['ffmpeg', '-i', vfname, '-vf', f'subtitles={sfname_esc}', res_fname, '-y']
     cmd = [
         'ffmpeg', '-i', vfname, '-i', sfname, 
-        '-c', 'copy', '-c:s', 'copy', 
-        '-metadata:s:s:0', 'language=chi', 
+        '-c:a', 'copy', '-c:v', 'copy', '-c:s', 'copy', 
         res_fname, '-y',
     ]
     print(f'cmd: {cmd}')
