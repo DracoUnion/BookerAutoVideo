73c71d388a56ee51db471f1c519278d8dcddf731
wizardforcel <562826179@qq.com>
2024-04-04 15:54:43 +0800

    2024-04-04 15:54:43

--- BookerAutoVideo/util.py
+++ BookerAutoVideo/util.py
@@ -503,7 +503,7 @@ def md2lines(cont):
             lines[i] =  l + '。'
     # 合并较短的段落
     for i in range(1, len(lines)):
-        if len(lines[i - 1]) < 15:
+        if len(lines[i - 1]) < 35:
             lines[i] = lines[i - 1] + lines[i]
             lines[i - 1] = ''
     lines = [l for l in lines if l]
